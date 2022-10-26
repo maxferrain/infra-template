83172d445cdfe35fdc335ea64fe603a17ced2670
maximilienk <mkru055@gmail.com>
2022-10-26 13:31:07 +0300

    Update

---
+++ build.js
@@ -0,0 +1,32 @@
+const fetch = require("node-fetch")
+const exec = require('@actions/exec')
+const github = require('@actions/github')
+require("dotenv").config()
+
+const {OAUTH, ORG_ID, ISSUE_ID, HOST} = process.env
+
+const headers = {
+    Authorization: `OAuth ${OAUTH}`,
+    "X-Org-ID": ORG_ID,
+}
+
+const build = async () => {
+    const currentTag = github.context.payload.ref.replace("refs/tags/", "") ?? ""
+    console.log(`currentTag ${currentTag}`)
+
+    await exec.exec('docker', ['build', '-t', `app:${currentTag}`, '.']);
+    console.info("Image build");
+
+    await fetch(`${HOST}/v2/issues/${ISSUE_ID}/comments`, {
+        method: "POST",
+        headers,
+        body: JSON.stringify({
+            text: `Собрали образ с тегом ${currentTag}`
+        })
+    });
+
+    console.info("Comment added");
+}
+
+build()
+    .then(() => console.log("Success build image"))
