d5a11c8dc99d025b35f0fc4a9ea8123b7bca2456
maximilienk <mkru055@gmail.com>
2022-10-26 13:16:40 +0300

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import React, {FC} from 'react'
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const App:FC<> = () => {
+function App() {
   return (
     <div className="App">
       <header className="App-header">
---
+++ updateTicketInfo.js
@@ -0,0 +1,81 @@
+const fetch = require("node-fetch")
+const exec = require('@actions/exec')
+const github = require('@actions/github')
+require("dotenv").config()
+
+const {OAUTH, ORG_ID, ISSUE_ID, HOST} = process.env
+
+const headers = {
+    Authorization: `OAuth ${OAUTH}`,
+    "X-Org-ID": ORG_ID,
+}
+
+const execute = async (command, args) => {
+    let output = ""
+    let error = ""
+
+    const options = {}
+    options.listeners = {
+        stdout: (data) => {
+            output += data.toString();
+        },
+        stderr: (data) => {
+            error += data.toString();
+        }
+    };
+
+    await exec.exec(command, args, options)
+
+    if (error) {
+        throw new Error(`Unable to execute ${command}`)
+    }
+    return output
+}
+
+
+const getTags = async () => {
+    return (await execute('git', ['tag'])).split("\n")
+        .filter(Boolean)
+        .sort((a, b) => {
+            const aVal = parseInt(a.replace("rc-0.0.", ""), 10);
+            const bVal = parseInt(b.replace("rc-0.0.", ""), 10);
+            return aVal - bVal;
+        });
+}
+
+const getCommitsInfo = async (tag) => {
+    const tags = await getTags()
+    const currentIndex = tags.indexOf(tag)
+    const commitsFilter = tags.length === 1 ? tag : `${tags[currentIndex - 1]}...${tag}`;
+    const releaseCommits = await execute('git', ['log', '--pretty=format:"%H %an %s"', commitsFilter]);
+    return releaseCommits.replace(/"/g, "");
+}
+
+
+const updateTicketInfo = async () => {
+    const currentTag = github.context.payload.ref.replace("refs/tags/", "") ?? ""
+    console.log(`currentTag ${currentTag}`)
+    const commits = await getCommitsInfo(currentTag)
+
+    const pusher = github.context.payload.pusher?.name ?? ""
+    console.log(`Pusher ${pusher}`)
+    const date = new Date().toLocaleDateString()
+    console.log(`Date ${date}`)
+
+    const summary = `Релиз №${currentTag.replace("rc-", "")} от ${date}`;
+    const description = `Ответственный за релиз: ${pusher}\n---\nКоммиты, попавшие в релиз:\n${commits}`;
+
+    await fetch(`${HOST}/v2/issues/${ISSUE_ID}`, {
+        method: "PATCH",
+        headers,
+        body: JSON.stringify({
+            // summary,
+            description
+        })
+    })
+}
+
+
+updateTicketInfo()
+    .then(() => console.log('Successful updating'))
+    .catch(() => console.log('Error occurred'))
